bc18ab0ded81e8199d9e2a07f2a609ac6f6b3a29
Jeongwon Kim <mickykim4@gmail.com>
2024-06-08 22:02:22 +0900

    implemented onboarding screen

--- lib/onboarding/data_onboarding.dart
+++ lib/onboarding/data_onboarding.dart
@@ -2,25 +2,22 @@ import 'model_onboarding.dart';
 
 class AppStrings {
   AppStrings._();
-  static const String onboardingOneTitle =
-      'Empower Your Home, Simplify Your Life';
+  static const String onboardingOneTitle = '선생님, 학생의 프로필을\n한눈에 확인하세요';
   static const String onboardingOneSubtitle =
-      'Transform your living space into a smarter, more connected home with Smartome. All at your fingertips.';
-  static const String onboardingTwoTitle =
-      'Effortless Control, Automate, & Secure';
+      '스타크래프트 립버전 1.16.1다운 있을 것 같았다. 그건 실로 벅찬 감격이었다.';
+  static const String onboardingTwoTitle = '리뷰와 같은 자세한 정보로 안전하게 과외를 성사시키세요';
   static const String onboardingTwoSubtitle =
-      'Smartome empowers you to control your devices, & automate your routines. Embrace a world where your home adapts to your needs';
-  static const String onboardingThreeTitle =
-      'Efficiency that Saves, Comfort that Lasts.';
+      '입구가 녹슬어 엉겨붙은 문을 열어 부지내를 마차가 스타크래프트 립버전 1.16.1다운 저택으로 향하는 길만은 어떻게든';
+  static const String onboardingThreeTitle = '바로 채팅을 보내고 과외를 확정하세요';
   static const String onboardingThreeSubtitle =
-      "Take control of your home's energy usage, set preferences, and enjoy a space that adapts to your needs while saving power.";
+      "장난기 어린 의 말에 휴스턴은 멍해져 있다가 크게 스타 립버전 1.16.1다운 터트렸다. 귀족들이나 스타크래프트 립버전 1.16.1다운";
 }
 
 class AppAssets {
   AppAssets._();
-  static const String onboardOneLight = 'assets/image/onboard1Light.png';
-  static const String onboardTwoLight = 'assets/image/onboard2Light.png';
-  static const String onboardThreeLight = 'assets/image/onboard3Light.png';
+  static const String onboardOneLight = 'assets/image/on1.png';
+  static const String onboardTwoLight = 'assets/image/on2.png';
+  static const String onboardThreeLight = 'assets/image/on3.png';
 }
 
 final List<OnboardingModel> onboardingData = [
